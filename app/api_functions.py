8611f7b822621d28e72d9d016b210312566f5c79
leterDieu <leterdieu@proton.me>
2025-10-15 21:29:36 +0300

    chat analysis v1

--- app/api_functions.py
+++ app/api_functions.py
@@ -1,18 +1,27 @@
-import requests as req
-import json
+'''API functions'''
 
+import json
+import requests as req
 
 API_URL = 'https://api.opendota.com/api/'
-large_match_id = 10 ** 12
+LARGE_MATCH_ID = 10**12
 
 
-def get_100_match_ids_lower_than_n(first_match_id: int = large_match_id) -> list[int]:
-    matches = json.loads(req.get(
-        API_URL + 'parsedMatches',
-        params={'less_than_match_id': first_match_id}).text)
+def get_100_match_ids_lower_than_n(
+        first_match_id: int = LARGE_MATCH_ID) -> list[int]:
+    '''gets 100 match ids from open dota which is a limit'''
+    matches = json.loads(
+        req.get(API_URL + 'parsedMatches',
+                params={
+                    'less_than_match_id': first_match_id
+                },
+                timeout=1).text)
     return [el['match_id'] for el in matches]
 
-def get_m_matches_lower_than_n(number_of_matches: int, first_match_id: int = large_match_id) -> list[int]:
+
+def get_m_matches_lower_than_n(
+        number_of_matches: int,
+        first_match_id: int = LARGE_MATCH_ID) -> list[int]:
     '''number of matches should be divisible by 100'''
     match_ids = []
     for _ in range(number_of_matches // 100):
@@ -20,25 +29,22 @@ def get_m_matches_lower_than_n(number_of_matches: int, first_match_id: int = lar
         first_match_id = match_ids[-1]
     return match_ids
 
+
 def get_match_info(match_id: int) -> dict:
-    match = json.loads(req.get(API_URL + f'matches/{match_id}').text)
+    '''gets match info'''
+    match = json.loads(
+        req.get(API_URL + f'matches/{match_id}', timeout=1).text)
     return match
 
-def get_match_chat(match_id: int) -> list:
-    chat = get_match_info(match_id)['chat']
-    chat_messages = []
-    for entry in chat:
-        if entry['type'] == 'chat':
-            chat_messages.append(entry)
-
-    return chat_messages
 
 def write_match_ids_to_file(match_ids: list[int], filename: str) -> None:
+    '''write ids to file'''
     with open('app/' + filename, 'w', encoding='utf-8') as file:
         file.write(json.dumps(match_ids))
-    return None
-    
-def get_and_write_match_ids_to_file(number_of_matches: int, filename: str) -> None:
+
+
+def get_and_write_match_ids_to_file(number_of_matches: int,
+                                    filename: str) -> None:
+    '''gets and writes ids to file'''
     match_ids = get_m_matches_lower_than_n(number_of_matches)
     write_match_ids_to_file(match_ids, filename)
-    return None
