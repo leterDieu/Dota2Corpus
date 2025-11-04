91acd24c3b5058bfbf076ae98eee55128acd2eac
HePa3Pa6oT4uk <kenarskijila@gmail.com>
2025-11-04 11:48:34 +0300

    Updated toxity_analysis and requirements

--- app/toxity_analysis.py
+++ app/toxity_analysis.py
@@ -1,15 +1,20 @@
-from detoxify import Detoxify
+import torch
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
 from api_functions import get_match_info
 from chat_analysis import get_match_chat
 
-
+tokenizer = AutoTokenizer.from_pretrained('textdetox/bert-multilingual-toxicity-classifier')
+model = AutoModelForSequenceClassification.from_pretrained('textdetox/bert-multilingual-toxicity-classifier')
 def count_toxity(a):
     toxity_dict = {}
     for i in a:
+        batch = tokenizer.encode(i['key'], return_tensors="pt")
+        output = model(batch)
+        probs = torch.softmax(output.logits, dim=-1)
         if i['slot'] not in toxity_dict.keys():
-            toxity_dict[str(i['slot'])] = [Detoxify('multilingual').predict(i['key'])['toxicity'], 1]
+            toxity_dict[str(i['slot'])] = [probs.tolist()[0][1], 1]
         else:
-            toxity_dict[str(i['slot'])][0] += Detoxify('multilingual').predict(i['key'])['toxicity']
+            toxity_dict[str(i['slot'])][0] += probs.tolist()[0][1]
             toxity_dict[str(i['slot'])][1] += 1
     for i in toxity_dict:
         toxity_dict[i] = toxity_dict[i][0] / toxity_dict[i][1]
